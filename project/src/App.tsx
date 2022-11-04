c1b90ad770e1df3d1b3a3bade776c11154a78429
mikkiklain <mikkiklain@gmail.com>
2022-11-04 05:13:00 +0300

    [develop] мелкий фикс [develop]

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -4,7 +4,7 @@ import styles from './App.module.scss'
 function App() {
   return (
     <div className={styles.app}>
-      <p className='text-red-500'>test text</p>
+      <p>test text</p>
     </div>
   )
 }
