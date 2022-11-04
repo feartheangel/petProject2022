8cd29b3d2f67e9c3174d2dbf6bfa628a63f0929a
mikkiklain <mikkiklain@gmail.com>
2022-11-05 01:36:09 +0300

    [feature/main-1] фикс [feature/main-1]

--- project/src/components/basic/header/Header.tsx
+++ project/src/components/basic/header/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Header = () => {
-  return <div className='h-full'>Header block</div>
+  return <div className='w-full'>Header block</div>
 }
 
 export default Header
